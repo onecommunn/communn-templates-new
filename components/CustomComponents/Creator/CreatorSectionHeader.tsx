98069941ac66d8aadba80e81acfdc71dce54c75d
communninterns <interns@communn.io>
2025-10-07 09:16:10 +0530

    Build check Update

--- components/CustomComponents/Creator/CreatorSectionHeader.tsx
+++ components/CustomComponents/Creator/CreatorSectionHeader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 interface ICreatorSectionHeader {
   title: string;
   description?: string;
-  textColor: string;
+  textColor?: string;
 }
 
 const CreatorSectionHeader = ({
