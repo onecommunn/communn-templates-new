119d7ea9dfa75ebb1d9bae206f56f2ccd9bb85f4
communninterns <interns@communn.io>
2025-10-27 10:07:14 +0530

    Martivo Build Fix

---
+++ app/martivo/layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { CMSProvider } from "./CMSProvider.client";
+
+const EMPTY_BUNDLE = {
+  header: null,
+  footer: null,
+  home: null,
+};
+
+const MartivoLayoyt = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <CMSProvider initialBundle={EMPTY_BUNDLE} initialLoading={true}>
+      {children}
+    </CMSProvider>
+  );
+};
+
+export default MartivoLayoyt;
